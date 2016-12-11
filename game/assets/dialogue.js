04d7a4ebc11699d4ea9895bc3f42cda0adef935f
Unknown <kirogata@gmail.com>
2016-12-11 14:22:04 -0800

    Push dialogue attempt 2

---
+++ game/assets/dialogue.js
@@ -0,0 +1,82 @@
+var DIALOGUE = {
+	characterdialogue: [
+		"I can't do it.",
+		"I shouldn't do it.",
+		"Why should I do it?",
+		"They just don't understand.",
+		"How could they understand?",
+		"They don't want to understand.",
+		"I don't want to see them.",
+		"They don't want to see me.",
+		"I'm safe here.",
+		"No matter how hard I try nothign works.",
+		"What about me is so different?",
+		"Why don't people see me?",
+		"<i><b>So what is the point of even trying?</b></i> Yeah. I just need to get by. Everything I need for that is in here.",
+		"I just have to get through the day.",
+
+	],
+	irlFriend: [
+		"I finally found you! It’s been awhile since school hasn’t it? I tried visiting you at the company that you told me took you on, but they were very confused when I asked for you.\
+		 <i>You liar.</i> The HR manager barely even remembered your name! <i>How pathetic is that!?</i>",
+		"I went looking for you, but your family was hard to get a hold of and they hadn’t heard from you in a long time either. \
+		<i>Even though they haven’t even tried because of how ashamed they are of you.</i> They are worried about you. Do you want to go see them? <i>So I can rub your failures in their face?</i>",
+		"Hey, I brought some food. It’s not much, but it doesn't seem like you are eating too well. <i>How pathetic are you to not even eat right?</i>\
+		 Do you have time, or should I just leave it here? <i>Like the rest of the trash here?</i>",
+		 "It’s been so long since we last sat down like this. What happened to the good old days? It’s like we graduated school and entered completely different worlds. <i>I ended up being a success of course.</i>\
+		 Sometimes I wonder if it’s really even you. Do you want to go to that bar we used to hit up where you got turned down by the server? <i>I’d love to see her reaction to you showing back up now!</i>",
+		 "Hey! I have great news, a tech company is hiring! They want applicants with a Masters degree and 4 years work experience in a related field <i>which you don’t have</i>, but that’s usually just rubbish right?\
+		  You should apply! <i>It will be a huge waste of time and really funny!</i>",
+		  "Do you have a minuet? <i>Of course you do!</i> I just got a promotion at work and I wanted to <i>brag</i> celebrate! Some of my coworkers will be there too. They are all really fun people, making me laugh <i>at you</i> all the time.",
+		  "Have you thought about going back to school and getting that degree you were talking about? <i>I mean what else are you doing with your waste of a life?</i> Maybe now is a good time to start?",
+		  "Don’t you want to meet new people? <i>So you can lie to them until they find out what you are really like and abandon you.</i> A stranger is just a friend that you haven’t met yet!",
+		  "I saw you family the other day. They wanted me to let you know that you can come home. <i>They can’t stand the thought of people finding out about you.</i> They are worried about <i>ashamed of</i> you.",
+		  "Am I the only one who has come to visit you? <i>Am I wasting my time trying to reconnect with you?</i>",
+		  "The store down the street is hiring! It’s only part time too! <i>So after taxes you’ll end up losing money AND have to interact with people who will hate you!</i> It’s a win-win!",
+		  "Are you getting enjoyment from living this way? <i>Does this give you meaning and fulfillment?</i> What would you rather be doing?",
+
+	],
+	virtualFriend: [
+	"You always seems to be online. <i>Isn’t the anything better you can do?</i> Don’t you have to work? Do you work from home?",
+	"My day has been Terrible! I don’t even want to talk about it. How was your day? </i>I want to hear how you did nothing all day.<i>",
+	"I just wanted to say that I really appreciate you always taking the time to talk to me. It always cheers me up and you always know what to say. <i> Are you sure you can keep this up?</i>",
+	"How did you get so good at that game? I heard it was really hard to get as far as you are. <i>So all you do is play games?</i>",
+	"When was the last time you went to your favorite restaurant? <i>You know, where you can feel everyone looking down on you?</i> I love a noodle shop down the street!",
+	"What is your favorite game? <i>I want to judge your thoughts on it.</i> There are so many out there, and I just don’t have time for them all. <i>What about you?</i>",
+	"Do you watch a lot of tv? <i>Do you get out at all?</i> Do you go to the movies?",
+	"What is your dream? <i>You know, the thing that you gave up before you even started!</i>",
+	"Why don’t you ever respond when I ask to meet up with you? <i>Give me a reason to stop wasting my time with you.</i>",
+	"Is there anything different you wish you could do about your life? <i>I mean, aside from what you are doing now.</i>",
+	"What do you think it means to be an adult? <i>What do you need to grow?</i>",
+
+	],
+	endingwithfriend: [
+	"Hikikomori(Shut in Syndrome) is a growing phenomenon that affects hundreds of thousands of people across the world. While prevalent in Japan and South Korea there are reported cases of incidents occurring worldwide, with more reported every year.\
+	 It is usually triggered by an event relating to some form of rejection or being treated as a metric. As a result many of these people are unable to accept the kindness of those close to them, and rarely encounter others who would think to treat them with dignity. \
+	Direct metrics are exceedingly hard to track because of the nature of those who encounter this state, and that many families look to keep them hidden out of fear for their family's reputation.\ 
+	No one who is “Hikikomori” is happy with how they are living their life, but they generally are unable to see a way out of their situation. They are generally exceedingly numb emotionally as a way of protecting themselves, caught in an endless cycle of\
+	 existing “outside the norm” with no way they can see to forge ahead. Being seen for who they are, not what they have done seems to be one of the largest factors in the process of their reintegration into society.\
+	This phenomenon shows the impact of our actions when we stop seeing people for who they are instead of what they are. In most cases, one person can make all of the difference.",
+
+	],
+	endingwithdigitalfriend: [
+	"Hikikomori(Shut in Syndrome) is a growing phenomenon that affects hundreds of thousands of people across the world. While prevalent in Japan and South Korea there are reported cases of incidents occurring worldwide, with more reported every year.\
+	 It is usually triggered by an event relating to some form of rejection or being treated as a metric. As a result many of these people are unable to accept the kindness of those close to them, and rarely encounter others who would think to treat them with dignity. \
+	Direct metrics are exceedingly hard to track because of the nature of those who encounter this state, and that many families look to keep them hidden out of fear for their family's reputation.\ 
+	No one who is “Hikikomori” is happy with how they are living their life, but they generally are unable to see a way out of their situation. They are generally exceedingly numb emotionally as a way of protecting themselves, caught in an endless cycle of\
+	 existing “outside the norm” with no way they can see to forge ahead. Being seen for who they are, not what they have done seems to be one of the largest factors in the process of their reintegration into society.\
+	 Some of these people seek human interaction via social media or the internet, keeping the world at a safe distance. But their desire to be accepted and connected is evident through these actions. The value of strong relationships,\
+	  regardless of the medium, can help build a person's self worth and give them the courage to face the world.",
+
+	],
+	endingalone: [
+	"Hikikomori(Shut in Syndrome) is a growing phenomenon that affects hundreds of thousands of people across the world. While prevalent in Japan and South Korea there are reported cases of incidents occurring worldwide, with more reported every year.\
+	 It is usually triggered by an event relating to some form of rejection or being treated as a metric. As a result many of these people are unable to accept the kindness of those close to them, and rarely encounter others who would think to treat them with dignity. \
+	Direct metrics are exceedingly hard to track because of the nature of those who encounter this state, and that many families look to keep them hidden out of fear for their family's reputation.\ 
+	No one who is “Hikikomori” is happy with how they are living their life, but they generally are unable to see a way out of their situation. They are generally exceedingly numb emotionally as a way of protecting themselves, caught in an endless cycle of\
+	 existing “outside the norm” with no way they can see to forge ahead. Being seen for who they are, not what they have done seems to be one of the largest factors in the process of their reintegration into society.\
+	 What do people become when they are not treated as people? Many countries raise their children with a set path for life, and few treat kindly those that stray from it. Few people stay as Hikikomori for their entire live, but there is no way to get back the time that was lost.\
+	  Even though there are not many answers now, the subject needs to be discussed as an issue for the world, not just as a cultural phenomenon exclusive to Japan.",
+	  
+	]
+}
